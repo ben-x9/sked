d5870e45abfc2018da3686c977ba9417ba70a9ec
Benjamin James <ben.jx9@gmail.com>
2022-08-25 05:24:30 +0900

    Display posts with filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
+import { useState } from "react"
 import "./App.css"
 import PostView from "./components/PostView"
 import usePosts from "./hooks/usePosts"
 
-
 export default function App() {
+  const [filter, setFilter] = useState({ IG: false, FB: false })
   const { isLoading, error, data: posts } = usePosts()
-  console.log("posts", posts)
   return (
     <div className="App">
+      <div className="filter">
+        <Checkbox
+          label="Select All Accounts"
+          name="all"
+          checked={filter.IG && filter.FB}
+          onChange={() =>
+            setFilter(filter =>
+              filter.FB && filter.IG
+                ? { IG: false, FB: false }
+                : { IG: true, FB: true }
+            )
+          }
+        />
+        <Checkbox
+          label="FB"
+          name="fb"
+          checked={filter.FB}
+          onChange={() => setFilter(filter => ({ ...filter, FB: !filter.FB }))}
+        />
+        <Checkbox
+          label="IG"
+          name="ig"
+          checked={filter.IG}
+          onChange={() => setFilter(filter => ({ ...filter, IG: !filter.IG }))}
+        />
+      </div>
       {isLoading ? (
         <div className="loading">Loading...</div>
       ) : error ? (
         <div className="error">{error.message}</div>
       ) : (
-        <div>
-          {posts?.map(post => (
-            <PostView {...post} />
-          ))}
+        <div className="posts">
+          {posts
+            ?.filter(post => filter[post.accountId])
+            .map(post => (
+              <PostView {...post} key={post.id} />
+            ))}
         </div>
       )}
     </div>
   )
 }
 
-
+const Checkbox = ({
+  name,
+  label,
+  onChange,
+  checked,
+}: {
+  name: string
+  label: string
+  onChange: () => void
+  checked: boolean
+}) => (
+  <div className="checkbox" onClick={onChange}>
+    <input type="checkbox" name={name} checked={checked} />
+    <label htmlFor={name}>{label}</label>
+  </div>
+)
--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -1,9 +1,17 @@
+import dayjs from "dayjs"
 import Post from "../models/post"
+import css from "./PostView.module.css"
 
 export default function PostView(post: Post) {
   return (
-    <div>
-      <div key={post.id}>{post.id}</div>
+    <div className={css.postView}>
+      {/* <div key={post.id}>{post.id}</div> */}
+      <div className={css.date}>
+        {dayjs(post.timestamp).format("ddd D  MMM, h:mm A")}
+      </div>
+      <div>{post.accountName}</div>
+      <img src={post.imageUrl} />
+      <div>{post.caption}</div>
     </div>
   )
 }
